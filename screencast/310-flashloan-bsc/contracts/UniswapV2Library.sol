36d4e9d386d33d99a3fe4c3940686dabe26d22e7
knowhope <88401383+knowhope@users.noreply.github.com>
2021-08-03 23:42:06 +0300

    Update UniswapV2Library.sol

--- screencast/310-flashloan-bsc/contracts/UniswapV2Library.sol
+++ screencast/310-flashloan-bsc/contracts/UniswapV2Library.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.0;
 
 import './interfaces/IUniswapV2Pair.sol';
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/b0cf6fbb7a70f31527f36579ad644e1cf12fdf4e/contracts/utils/math/SafeMath.sol";
 
 library UniswapV2Library {
     using SafeMath for uint;
